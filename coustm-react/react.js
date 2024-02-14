b87a7438b7c2580a01b448df8b08151e8e8ac5e7
pushpa rajput <pr4541285@gmail.com>
2024-02-14 11:12:25 +0530

    first commit

---
+++ coustm-react/react.js
@@ -0,0 +1,22 @@
+function customRender(reactElement, container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  /*domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  container.appendChild(domElement); */
+  for (const key in reactElement.props) {
+    if (key === "children") continue;
+    domElement.setAttribute(`${key}`, reactElement.props[key]);
+    container.appendChild(domElement);
+  }
+}
+const reactElement = {
+  type: "a",
+  props: {
+    href: "http://google.com",
+    target: "_blank",
+  },
+  children: "click me to visit google",
+};
+const mainContainer = document.getElementById("root");
+customRender(reactElement, mainContainer);
